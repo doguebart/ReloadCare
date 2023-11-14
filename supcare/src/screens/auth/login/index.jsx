2a7e957246038ddd1366dd3b9c075ead498f4005
doguebart <douglasweelber.2004@outlook.com>
2023-11-14 14:50:48 -0300

    feat: creating login page

--- supcare/src/screens/auth/login/index.jsx
+++ supcare/src/screens/auth/login/index.jsx
@@ -1,12 +1,95 @@
-import React from 'react';
-import { Container, Title } from './styles';
+import React, { useState } from "react";
+import {
+  Container,
+  Form,
+  ScrollViewContainer,
+  InputContainer,
+  Text,
+  Title,
+} from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import Label from "../../../components/form/label";
+import InputComponent from "./../../../components/form/input";
+import ButtonComponent from "./../../../components/form/button";
+import LinkComponent from "./../../../components/form/link";
 
-const Login = () => {
-    return (
-        <Container>
+const Register = () => {
+  const navigation = useNavigation();
+  const [formData, setFormData] = useState({ email: "", senha: "" });
+  const [validationPerformed, setValidationPerformed] = useState(false);
+  const [errors, setErrors] = useState({});
 
-        </Container>
-    )
-}
+  const handleInputChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
 
-export default Login;
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.email) {
+      newErrors.email = "E-mail é obrigatório";
+    } else {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(formData.email)) {
+        newErrors.email = "E-mail inválido";
+      }
+    }
+
+    if (!formData.senha) {
+      newErrors.senha = "Senha é obrigatória";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleLogin = () => {
+    setValidationPerformed(true);
+
+    if (validateForm()) {
+    } else {
+      console.log("Formulário inválido, corrija os erros:", errors);
+    }
+  };
+
+  const goToRegister = () => {
+    navigation.navigate("Register");
+  };
+
+  return (
+    <Container>
+      <Title>ENTRAR</Title>
+      <Form>
+        <ScrollViewContainer>
+          <InputContainer>
+            <Label>E-mail</Label>
+            <InputComponent
+              type="email"
+              name="email"
+              onChangeText={(value) => handleInputChange("email", value)}
+              placeholder="Insira seu nome e-mail"
+              errorMessage={errors.email}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label>Senha</Label>
+            <InputComponent
+              type="password"
+              name="senha"
+              secureTextEntry={true}
+              onChangeText={(value) => handleInputChange("senha", value)}
+              placeholder="Insira sua senha"
+              errorMessage={errors.senha}
+            />
+          </InputContainer>
+        </ScrollViewContainer>
+        <ButtonComponent onPress={handleLogin}>Entrar</ButtonComponent>
+        <LinkComponent style={{ textAlign: "center" }} onPress={goToRegister}>
+          Ainda não tem uma conta? <Text>Criar</Text>
+        </LinkComponent>
+      </Form>
+    </Container>
+  );
+};
+
+export default Register;
