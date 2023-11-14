b00423955545edaf1ec33f52712e1f0e71f4d783
doguebart <douglasweelber.2004@outlook.com>
2023-11-14 14:34:51 -0300

    updating router file

---
+++ supcare/src/routes/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Login from "../screens/auth/login";
+import Register from "../screens/auth/register";
+import Password from "../screens/auth/register/password";
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Password"
+        component={Password}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Routes;
